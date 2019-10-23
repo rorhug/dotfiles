2b16620f6e45bb2745f34c64b8c38f49ec11ae7a
Rory Hughes <RoryDH@users.noreply.github.com>
2019-10-23 12:05:03 +0100

    fix setup link

--- setup_link.zsh
+++ setup_link.zsh
@@ -1,8 +1,10 @@
 ln -sf ~/dotfiles/gitconfig.config ~/.gitconfig
 
+/bin/rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 ln -sfh ~/dotfiles/st3-packages/User ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
 
 ln -sf ~/dotfiles/zshrc.zsh ~/.zshrc
 
+/bin/rm -rf ~/Library/Application\ Support/Code/User
 ln -sfh ~/dotfiles/vscode/User ~/Library/Application\ Support/Code
 
